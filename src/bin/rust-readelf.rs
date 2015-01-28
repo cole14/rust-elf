f894a8f3114668edd82839d60247da1bf5c6a465
Christopher Cole <chris.cole.09@gmail.com>
2015-01-27 23:50:20 -0800

    Move ELF type definitions to a types module

--- src/bin/rust-readelf.rs
+++ src/bin/rust-readelf.rs
@@ -3,7 +3,7 @@ extern crate elf;
 use std::default;
 
 fn main() {
-    let ehdr: elf::Elf32Ehdr = default::Default::default();
+    let ehdr: elf::types::Elf32Ehdr = default::Default::default();
     println!("{:?}", ehdr);
     println!("{}", ehdr);
 }
