c2dd168fe69c44b780dd3da07855e4cea28032e4
Christopher Cole <chris.cole.09@gmail.com>
2015-02-09 20:20:37 -0800

    Add types for ELF Symbols
    Symbol encapsulates both Elf32_Sym and Elf64_Sym

--- src/types.rs
+++ src/types.rs
@@ -694,3 +694,112 @@ impl fmt::Display for SectionHeader {
     }
 }
 
+#[derive(Copy)]
+pub struct SymbolType(pub u8);
+/// Unspecified symbol type
+pub const STT_NOTYPE : SymbolType = SymbolType(0);
+/// Data object symbol
+pub const STT_OBJECT : SymbolType = SymbolType(1);
+/// Code object symbol
+pub const STT_FUNC : SymbolType = SymbolType(2);
+/// Section symbol
+pub const STT_SECTION : SymbolType = SymbolType(3);
+/// File name symbol
+pub const STT_FILE : SymbolType = SymbolType(4);
+/// Common data object symbol
+pub const STT_COMMON : SymbolType = SymbolType(5);
+/// Thread-local data object symbol
+pub const STT_TLS	 : SymbolType = SymbolType(6);
+/// Indirect code object symbol
+pub const STT_GNU_IFUNC : SymbolType = SymbolType(10);
+
+impl fmt::Display for SymbolType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match *self {
+            STT_NOTYPE => "unspecified",
+            STT_OBJECT => "data object",
+            STT_FUNC => "code object",
+            STT_SECTION => "section",
+            STT_FILE => "file name",
+            STT_COMMON => "common data object",
+            STT_TLS => "thread-local data object",
+            STT_GNU_IFUNC => "indirect code object",
+            _ => "Unknown",
+        };
+        write!(f, "{}", str)
+    }
+}
+
+#[derive(Copy)]
+pub struct SymbolBind(pub u8);
+/// Local symbol
+pub const STB_LOCAL : SymbolBind = SymbolBind(0);
+/// Global symbol
+pub const STB_GLOBAL : SymbolBind = SymbolBind(1);
+/// Weak symbol
+pub const STB_WEAK : SymbolBind = SymbolBind(2);
+/// Unique symbol
+pub const STB_GNU_UNIQUE : SymbolBind = SymbolBind(10);
+
+impl fmt::Display for SymbolBind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match *self {
+            STB_LOCAL => "local",
+            STB_GLOBAL => "global",
+            STB_WEAK => "weak",
+            STB_GNU_UNIQUE => "unique",
+            _ => "Unknown",
+        };
+        write!(f, "{}", str)
+    }
+}
+
+#[derive(Copy)]
+pub struct SymbolVis(pub u8);
+/// Default symbol visibility
+pub const STV_DEFAULT : SymbolVis = SymbolVis(0);
+/// Processor-specific hidden visibility
+pub const STV_INTERNAL : SymbolVis = SymbolVis(1);
+/// Hidden visibility
+pub const STV_HIDDEN : SymbolVis = SymbolVis(2);
+/// Protected visibility
+pub const STV_PROTECTED : SymbolVis = SymbolVis(3);
+
+impl fmt::Display for SymbolVis {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str = match *self {
+            STV_DEFAULT => "default",
+            STV_INTERNAL => "internal",
+            STV_HIDDEN => "hidden",
+            STV_PROTECTED => "protected",
+            _ => "Unknown",
+        };
+        write!(f, "{}", str)
+    }
+}
+
+pub struct Symbol {
+    /// Symbol name
+    pub name: String,
+    /// Symbol value
+    pub value: u64,
+    /// Symbol size
+    pub size: u64,
+    info: u8,
+    other: u8,
+}
+
+impl Symbol {
+    pub fn sym_type(&self) -> SymbolType {
+        SymbolType(self.info & 0xf)
+    }
+
+    pub fn sym_bind(&self) -> SymbolBind {
+        SymbolBind(self.info >> 4)
+    }
+
+    pub fn sym_vis(&self) -> SymbolVis {
+        SymbolVis(self.other & 0x3)
+    }
+}
+
