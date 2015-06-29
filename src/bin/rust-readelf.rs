f5398226d719fc50144545e80edd55ab419fe9ab
Richard Diamond <wichard@vitalitystudios.com>
2015-06-29 06:04:37 -0500

    Accept multiple arguments from `rust-readelf`.

--- src/bin/rust-readelf.rs
+++ src/bin/rust-readelf.rs
@@ -1,24 +1,35 @@
 
 extern crate elf;
 
-use std::path::Path;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
-    let path = Path::new("stress");
-    let file = match elf::File::open_path(&path) {
-        Ok(f) => f,
-        Err(e) => panic!("Error: {:?}", e),
+    let args: Vec<String> = env::args().collect();
+    let paths: Vec<PathBuf> = if args.len() == 1 {
+        vec!(From::from("stress"))
+    } else {
+        let mut i = args.into_iter();
+        i.next();
+        i.map(|arg| From::from(arg) )
+            .collect()
     };
-    println!("Debug-print ELF file:");
-    println!("{:?}", file);
-    println!("");
-    println!("Pretty-print ELF file:");
-    println!("{}", file);
+    for path in paths.into_iter() {
+        let file = match elf::File::open_path(&path) {
+            Ok(f) => f,
+            Err(e) => panic!("Error: {:?}", e),
+        };
+        println!("Debug-print ELF file:");
+        println!("{:?}", file);
+        println!("");
+        println!("Pretty-print ELF file:");
+        println!("{}", file);
 
-    println!("Getting the .text section");
-    let text = file.get_section(".text");
-    match text {
-        Some(s) => println!("shdr: {}", s),
-        None => println!("Failed to look up .text section!"),
+        println!("Getting the .text section");
+        let text = file.get_section(".text");
+        match text {
+            Some(s) => println!("shdr: {}", s),
+            None => println!("Failed to look up .text section!"),
+        }
     }
 }
